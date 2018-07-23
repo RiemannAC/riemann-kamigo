13270508a464d18ec510796ad3fa68ea85c5d92f
RiemannAC <dr.mactt@gmail.com>
2018-07-23 15:59:13 +0800

    Add kamigo#response_headers

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -14,4 +14,11 @@ class KamigoController < ApplicationController
   def request_body
     render plain: request.body
   end
+
+  def response_headers
+    response.headers['5566'] = 'QQ'# 資料很少，試增加自訂資料到 response headers
+    render plain: response.headers.to_h.map{ |key, value|
+      "#{key}: #{value}"
+    }.sort#.join{"\n"} 不加是 json 加了反沒換行
+  end
 end

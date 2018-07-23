8b1da40b1d281613e196aee714455f062b7feb35
RiemannAC <dr.mactt@gmail.com>
2018-07-23 15:21:11 +0800

    Add kamigo#request_headers

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -2,4 +2,12 @@ class KamigoController < ApplicationController
   def eat
     render plain: "吃土啦"
   end
+
+  def request_headers
+    render plain: request.headers.to_h.reject{ |key, value|
+      key.include?'.'
+    }.map { |key, value|
+      "#{key}: #{value}"
+    }.sort.join("\n")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'kamigo/eat', to: 'kamigo#eat'
+  get 'kamigo/request_headers', to: 'kamigo#request_headers'
 end
